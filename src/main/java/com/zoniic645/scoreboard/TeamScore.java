1563b8ef1dc9695926c93f9ada40262dc796bef3
ZoniIC645 <michael1930@naver.com>
2018-12-16 10:47:24 +0900

    색깔놀이

--- src/main/java/com/zoniic645/scoreboard/TeamScore.java
+++ src/main/java/com/zoniic645/scoreboard/TeamScore.java
@@ -8,7 +8,8 @@ import net.minecraft.world.storage.MapStorage;
 import net.minecraft.world.storage.WorldSavedData;
 
 public class TeamScore extends WorldSavedData {
-    public static final String ANSI_CYAN = "\u001B[36m";//색좀 줘보려고. 의미있는건 아님
+    public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+    public static final String ANSI_RESET = "\u001B[0m";//색좀 줘보려고. 의미있는건 아님
     private ForgeTeam team = null;
     private long score = 0;
 
@@ -33,14 +34,16 @@ public class TeamScore extends WorldSavedData {
 
     @Override
     public void readFromNBT(NBTTagCompound compound) {
-        System.out.println(ANSI_CYAN + "WorldSavedData : NBT데이터 뽑아서 적용시킹 : " + compound.getShort("teamUID") + ANSI_CYAN);
+        System.out.println(ANSI_WHITE_BACKGROUND + "WorldSavedData : NBT데이터 뽑아서 적용시킹 : " + compound.getShort("teamUID") + ANSI_RESET);
+
         team = Universe.get().getTeam(compound.getShort("teamUID")); //팀 UID뽑아서 적용시킴
         score = compound.getLong("score"); //스코어 뽑아서 적용시킴
     }
 
     @Override
     public NBTTagCompound writeToNBT(NBTTagCompound compound) {
-        System.out.println(ANSI_CYAN + "WorldSavedData : NBT 데이터에 저장함 : " + team.getUID() + ANSI_CYAN);
+        System.out.println(ANSI_WHITE_BACKGROUND + "WorldSavedData : NBT 데이터에 저장함 : " + team.getUID() + ANSI_RESET);
+
         compound.setShort("teamUID", team.getUID()); //팀을 저장할거니까 UID값을 넣어줌
         compound.setLong("score", score); //스코어니까 long을 넣어줌. 둘다 키값은 아무거나 넣음
         return compound;
@@ -49,8 +52,12 @@ public class TeamScore extends WorldSavedData {
     public static TeamScore getTeamScore(World world, ForgeTeam teamIn) {
         System.out.println("팀을 반환합니다");
         String teamscorenameIn = teamIn.toString();
+        if (world == null)
+            System.out.println("world가 null이잖아 빡대가리새끼야");
+        else
+            System.out.println("난 빡대가리가 아니였어");
         MapStorage storage = world.getMapStorage(); //맵스토리지를 가져왔어요
-        TeamScore Instance = (TeamScore) storage.getOrLoadData(TeamScore.class, teamscorenameIn); //스토리지에서 팀스코어를 꺼내서 넣어줘요x
+        TeamScore Instance = (TeamScore) storage.getOrLoadData(TeamScore.class, teamscorenameIn); //스토리지에서 팀스코어를 꺼내서 넣어줘요
         if (Instance == null) { //가져왔는데 null이니까 없다는거
             System.out.println("없어서 새로 하나 만듬");
             Instance = new TeamScore(teamscorenameIn, teamIn); //없으니까 일단 하나 만든다
